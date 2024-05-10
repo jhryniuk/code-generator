b98def7db24b86f2394a32f3353d3cf5c472c135
Mateusz Fruzynski <mattfruz@gmail.com>
2024-05-11 01:48:46 +0200

    Frontend added.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,102 @@
-import { React, useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import "./App.css";
 
-function App() {
-    const url = "http://nginx/api/codes";
-    const [data, setData] = useState([]);
+const App = () => {
+    const [id, setId] = useState(null);
+    const [code, setCode] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [dataFetched, setDataFetched] = useState(false);
+    const [error, setError] = useState<Error | null>(null);
 
-    const fetchInfo = () => {
-        return fetch(url)
-            .then((res) => res.json())
-            .then((d) => setData(d))
-    }
+    const sendPostRequest = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch('http://localhost/api/codes', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/ld+json',
+                    'Content-Type': 'application/ld+json'
+                },
+                body: JSON.stringify({ code: 'string' })
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to send POST request');
+            }
+
+            const responseData = await response.json();
+            const newId = responseData.id;
+            setId(newId);
+        } catch (error: any) {
+            setError(new Error(`Error: ${(error as Error).message}`));
+            setLoading(false);
+        }
+    };
 
     useEffect(() => {
-        fetchInfo();
-    }, []);
+
+        const fetchCode = async () => {
+            try {
+
+                if (id === null) return;
+                setLoading(true);
+
+                const response = await fetch(`http://localhost/api/codes/${id}`, {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/ld+json'
+                    }
+                });
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch code');
+                }
+
+                const codeData = await response.json();
+                console.log(codeData);
+                setCode(codeData.code);
+                setDataFetched(true);
+                setLoading(false);
+            } catch (error) {
+                setError(new Error(`Error: ${(error as Error).message}`));
+                setLoading(false);
+            }
+        };
+
+        if (id !== null) {
+            setTimeout(() => {
+                fetchCode();
+                setLoading(true);
+            }, 1000);
+        }
+    }, [id]);
 
     return (
-        <div className="App">
-            <h1 style={{ color: "green" }}>Using JavaScript inbuilt FETCH API</h1>
-            <center>
-                {data.map((dataObj, index) => {
-                    return (
-                        <div
-                            key={index} // Add a unique key for each element in the array
-                            style={{
-                                width: "15em",
-                                backgroundColor: "#35D841",
-                                padding: 2,
-                                borderRadius: 10,
-                                marginBlock: 10,
-                            }}
-                        >
-                            <p style={{ fontSize: 20, color: 'white' }}>{dataObj.name}</p>
-                        </div>
-                    );
-                })}
-            </center>
+        <div className="background">
+            <div className="content">
+                <button className="button" onClick={sendPostRequest}>Execute Code</button>
+                    {loading ? (
+                        <p>Loading...</p>
+                    ) : (
+                        <>
+                            {error ? (
+                                <p>{error.message}</p>
+                            ) : (
+                                <>
+                                    {dataFetched && (
+                                        <p>
+                                            ID: {id}
+                                            <br/>
+                                            Code: {code}
+                                        </p>
+                                    )}
+                                </>
+                            )}
+                        </>
+                    )}
+            </div>
         </div>
     );
-}
+};
 
 export default App;
